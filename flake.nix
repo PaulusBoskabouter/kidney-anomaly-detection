b4c29053e1edaf378003fb1926b00054437704c0
PaulusBoskabouter <43268349+PaulusBoskabouter@users.noreply.github.com>
2026-09-10 10:07:39 +0200

    initial project setup

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [ pkgs.python3 pkgs.uv ];
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+
+          pkgs.stdenv.cc.cc.lib   # libstdc++
+          pkgs.zlib
+          pkgs.cudaPackages.cudatoolkit
+          pkgs.cudaPackages.cuda_cudart
+          pkgs.libGL
+        ] + ":/run/opengl-driver/lib";
+      };
+    };
+}
