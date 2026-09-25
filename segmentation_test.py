2cb8d3332c10d13ce92c6b16f0932af42b444652
PaulusBoskabouter <43268349+PaulusBoskabouter@users.noreply.github.com>
2026-09-25 16:07:41 +0200

    hsv vs otsu

--- segmentation_test.py
+++ segmentation_test.py
@@ -11,33 +11,17 @@ for disease in root.iterdir():
 
 
 
-
-tile_slides(
-    slides,
-    tiling=TilingConfig(
-        requested_spacing_um=0.5,
-        requested_tile_size_px=224,
-        tolerance=0.07,
-        overlap=0.0,
-        min_coverage={"tissue": 0.1},
-    ),
-    segmentation=SegmentationConfig(method='otsu', downsample=64, sthresh=254, sthresh_up=255, mthresh=7, close=4),
-    preview=PreviewConfig(save_mask_preview=True, save_tiling_preview=False, downsample=64),
-    output_dir=Path(f"output/otsu"),
-)
-
-tile_slides(
-    slides,
-    tiling=TilingConfig(
-        requested_spacing_um=0.5,
-        requested_tile_size_px=224,
-        tolerance=0.07,
-        overlap=0.0,
-        min_coverage={"tissue": 0.1},
-    ),
-    segmentation=SegmentationConfig(method='hsv', downsample=64, sthresh=254, sthresh_up=255, mthresh=7, close=4),
-
-
-    preview=PreviewConfig(save_mask_preview=True, save_tiling_preview=False, downsample=64),
-    output_dir=Path(f"output/hsv"),
-)
+for method in ["otsu", "hsv"]:
+    tile_slides(
+        slides,
+        tiling=TilingConfig(
+            requested_spacing_um=0.5,
+            requested_tile_size_px=224,
+            tolerance=0.07,
+            overlap=0.0,
+            min_coverage={"tissue": 0.1},
+        ),
+        segmentation=SegmentationConfig(method=method, downsample=64, sthresh=254, sthresh_up=255, mthresh=7, close=4),
+        preview=PreviewConfig(save_mask_preview=True, save_tiling_preview=False, downsample=64),
+        output_dir=Path(f"segmentation/{method}"),
+    )
